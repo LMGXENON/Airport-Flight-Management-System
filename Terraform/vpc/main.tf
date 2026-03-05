f25b2c07a14cd32954ce60babc3a589cf48641cf
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-05 20:49:14 +0000

    added public subnets

--- Terraform/vpc/main.tf
+++ Terraform/vpc/main.tf
@@ -6,3 +6,10 @@ resource "aws_vpc" "gatus_vpc" {
   }
 
 }
+
+resource "aws_subnet" "public" {
+  count             = 2
+  vpc_id            = aws_vpc.gatus_vpc.id
+  cidr_block        = var.public_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+}
