91ad27b170264db3493fb5e7b1acb9c55116e9e0
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-09 21:12:29 +0000

    added subnets and variables

--- Terraform/vpc/main.tf
+++ Terraform/vpc/main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "gatus_vpc" {
+resource "aws_vpc" "afms_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
 
@@ -9,7 +9,14 @@ resource "aws_vpc" "gatus_vpc" {
 
 resource "aws_subnet" "public" {
   count             = 2
-  vpc_id            = aws_vpc.gatus_vpc.id
+  vpc_id            = aws_vpc.afms_vpc.id
   cidr_block        = var.public_cidrs[count.index]
   availability_zone = var.azs[count.index]
+}
+resource "aws_subnet" "private" {
+  count             = 2
+  vpc_id            = aws_vpc.afms_vpc.id
+  cidr_block        = var.private_cidrs[count.index]
+  availability_zone = var.azs[count.index]
+
 }
