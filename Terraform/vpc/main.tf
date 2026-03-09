0801341da8f4850ce0ff1ea4c8d154eec0213ed9
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-09 21:15:07 +0000

    fixed route association

--- Terraform/vpc/main.tf
+++ Terraform/vpc/main.tf
@@ -27,17 +27,22 @@ resource "aws_route_table" "public" {
     gateway_id = aws_internet_gateway.igw.id
   }
 }
-resource "aws_subnet" "private" {
-  count             = 2
-  vpc_id            = aws_vpc.afms_vpc.id
-  cidr_block        = var.private_cidrs[count.index]
-  availability_zone = var.azs[count.index]
-
+resource "aws_route_table_association" "pub-rt" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
-resource "aws_route_table" "public" {
+
+resource "aws_route_table" "private" {
+  count  = 2
   vpc_id = aws_vpc.afms_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = aws_nat_gateway.ngw[count.index].id
   }
+}
+resource "aws_route_table_association" "priv-rt" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
