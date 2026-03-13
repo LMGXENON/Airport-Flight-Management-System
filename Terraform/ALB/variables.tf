401158e73eaf7dbae8bc6026ca0e8778a8f25a0a
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-13 22:36:04 +0000

    added imported variables to alb

--- Terraform/ALB/variables.tf
+++ Terraform/ALB/variables.tf
@@ -1,3 +1,17 @@
 variable "vpc_id" {
   description = "vpc_id"
 }
+variable "public_subnet_ids" {
+
+  type = list(string)
+
+}
+
+variable "alb_sg_id" {
+  type = string
+}
+
+variable "certificate_arn" {
+  type = string
+
+}
