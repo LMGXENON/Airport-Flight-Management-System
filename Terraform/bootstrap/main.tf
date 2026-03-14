db52d85f6721c740c9c74156a3ce414b6cf6b11c
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-14 22:50:56 +0000

    added ecr lifecycle policy to expire old images

--- Terraform/bootstrap/main.tf
+++ Terraform/bootstrap/main.tf
@@ -7,3 +7,21 @@ resource "aws_ecr_repository" "afms_repo" {
     scan_on_push = true
   }
 }
+resource "aws_ecr_lifecycle_policy" "repo_policy" {
+  repository = aws_ecr_repository.afms_repo.name
+
+  policy = jsonencode({
+    rules = [{
+      rulePriority = 1
+      description  = "Expire old images"
+      selection = {
+        tagStatus   = "any"
+        countType   = "imageCountMoreThan"
+        countNumber = 10
+      }
+      action = {
+        type = "expire"
+      }
+    }]
+  })
+}
