2547c42b307d3c36c89b49f575d92fcf4ef1a4a0
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-16 23:13:30 +0000

    finished off s3 bootstrap config

--- AFMS/bootstrap/main.tf
+++ AFMS/bootstrap/main.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "tf-state" {
+resource "aws_s3_bucket" "tf_state" {
   bucket = "afms-state"
 
   lifecycle {
@@ -7,11 +7,21 @@ resource "aws_s3_bucket" "tf-state" {
 
 }
 resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-  bucket = aws_s3_bucket.tf-state.id
+  bucket = aws_s3_bucket.tf_state.id
 
   versioning_configuration {
     status = "Enabled"
   }
+}
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_state_encryption" {
+  bucket = aws_s3_bucket.tf_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+  
 }
 resource "aws_ecr_repository" "afms_repo" {
   name         = "afms-repo"
