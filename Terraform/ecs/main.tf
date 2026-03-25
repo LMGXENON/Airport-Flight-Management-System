8e8f363643ee4e3c21bea81f9a30a67024b4cb5e
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-25 12:19:22 +0000

    added ecs task definition

--- Terraform/ecs/main.tf
+++ Terraform/ecs/main.tf
@@ -7,3 +7,34 @@ resource "aws_cloudwatch_log_group" "afms" {
   name              = "afms_ecs_cloudwatch"
   retention_in_days = 7
 }
+resource "aws_ecs_task_definition" "afms-task" {
+  family                   = "afms_task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = var.execution_role_arn
+  container_definitions = jsonencode([
+    {
+      name  = "afms"
+      image = "${var.afms_image}:${var.afms_image_tag}"
+
+      portMappings = [{
+        containerPort = 8080
+        protocol      = "tcp"
+      }]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.afms.name
+          awslogs-stream-prefix = "ecs"
+          awslogs-region        = var.region
+        }
+      }
+    }
+  ])
+
+
+
+}
