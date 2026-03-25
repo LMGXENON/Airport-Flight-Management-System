5c3c8abf2095d9d318a0e7bf6281b509d726825f
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-25 17:44:40 +0000

    began adding module and variable references in root main tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -16,8 +16,17 @@ module "RDS" {
 module "ALB" {
   source = "./ALB"
   vpc_id = module.vpc.vpc_id
+  vpc_cidr_block = module.vpc.vpc_cidr_block
   public_subnet_ids = module.vpc.public_subnet_ids
   alb_sg_id         = module.security_groups.alb_sg_id
   certificate_arn   = module.Route53.certificate_arn
+
+
+}
+
+module "Route53" {
+  source = "./Route53"
+  alb_dns_name = module.ALB.alb_dns_name
+  alb_zone_id = module.ALB.alb_zone_id
 
 }
