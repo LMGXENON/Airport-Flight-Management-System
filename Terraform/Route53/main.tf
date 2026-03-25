cd563a0ca86b665f59e149a0f75c91fd420fd91b
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-25 17:40:33 +0000

    added cert validation

--- Terraform/Route53/main.tf
+++ Terraform/Route53/main.tf
@@ -12,3 +12,19 @@ resource "aws_acm_certificate" "cert" {
   domain_name       = "AFMS.com"
   validation_method = "DNS"
 }
+resource "aws_route53_record" "acm_cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_cert.domain_validation_options :
+    dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+
+  }
+  zone_id = "Z0404500D627T0ENSBZN"
+  name    = each.value.name
+  type    = each.value.type
+  ttl     = 60
+  records = [each.value.record]
+}
