d08ffef59fd6135a616d54d48114264efc8b26fb
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-25 23:32:55 +0000

    fixed egress rules to ecs sg

--- Terraform/ecs/main.tf
+++ Terraform/ecs/main.tf
@@ -81,6 +81,27 @@ resource "aws_security_group_rule" "ecs_in_from_alb" {
   to_port                  = var.afms_port
   protocol                 = "tcp"
 }
+
+resource "aws_security_group_rule" "ecs_out_to_ecr" {
+  type              = "egress"
+  security_group_id = aws_security_group.ecs_sg.id
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  description       = "Allow HTTPS to ECR and other services"
+}
+
+resource "aws_security_group_rule" "ecs_out_to_rds" {
+  type              = "egress"
+  security_group_id = aws_security_group.ecs_sg.id
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.0/16"]
+  description       = "Allow PostgreSQL to RDS"
+}
+
 output "aws_ecs_task_definition_arn" {
   value = aws_ecs_task_definition.afms-task.arn
 
