1ed52910271c24404ac2b6c7a5f3d00d6766ea99
Mubarek-Amin <aminmubarek@icloud.com>
2026-03-25 19:28:21 +0000

    made changes to main tf added iam role

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,6 @@
 
 module "vpc" {
   source = "./vpc"
-  
 }
 
 module "ecs" {
@@ -9,27 +8,49 @@ module "ecs" {
   vpc_id = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
   target_group_arn = module.ALB.target_group_arn
-  ecs_sg_id = module.security_groups.ecs_sg_id
+  alb_sg_id = module.ALB.alb_sg_id
   afms_image = "afms"
   afms_image_tag = "latest"
   region = "eu-west-2"
-  execution_role_arn = module.ecs_task_execution_role.execution_role_arn
- 
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
 }
 
 module "RDS" {
-  source = "./RDS"
+  source              = "./RDS"
+  vpc_id              = module.vpc.vpc_id
+  private_subnet_ids  = module.vpc.private_subnet_ids
+  ecs_sg_id           = module.ecs.ecs_sg_id
+  db_password         = var.rds_password
+  
+  depends_on = [module.ecs]
 }
 
 module "ALB" {
   source = "./ALB"
   vpc_id = module.vpc.vpc_id
-  vpc_cidr_block = module.vpc.vpc_cidr_block
   public_subnet_ids = module.vpc.public_subnet_ids
-  alb_sg_id         = module.security_groups.alb_sg_id
   certificate_arn   = module.Route53.certificate_arn
+}
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs-task-execution-role"
 
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 module "Route53" {
